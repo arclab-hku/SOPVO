6463f8b5a6584a96528406346add1b59dd921bde
rduan036 <ran.rd.duan@polyu.edu.hk>
2022-01-20 22:40:13 +0800

    update test bag

---
+++ bag/download_bag.sh
@@ -0,0 +1,5 @@
+# EUROC example
+wget http://robotics.ethz.ch/~asl-datasets/ijrr_euroc_mav_dataset/machine_hall/MH_05_difficult/MH_05_difficult.bag
+# T265 example
+# https://drive.google.com/file/d/120WtpXt8S25mX8qVsPO7OakMC8dvOlU9/view?usp=sharing
+
